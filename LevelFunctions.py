e329ca9ccb8222c5bb78070d4b81fc6136d8cc81
noahyohannes <noah.yohannes23@outlook.com>
2026-02-28 06:11:56 -0500

    update formatting

--- LevelFunctions.py
+++ LevelFunctions.py
@@ -6,15 +6,29 @@ BG_COLOR = pygame.Color(177,172,255)
 SAFE_AREA_COLOR = pygame.Color(114,222,110)
 BG_SQUARE_LENGTH = 60 
 
-def create_level(screen,safeAreas,finishArea,startPosition = pygame.Vector2(0,0),x=0,y=0):
-    if(x == 0 or y == 0):
+def create_level(
+    screen,
+    safeAreas,
+    finishArea,
+    startPosition = pygame.Vector2(0,0),
+    x=0,
+    y=0
+):
+    '''
+    Given a width and height, and
+    '''
+    if x == 0 or y == 0:
         raise ValueError("[create_level] one or more args not given!")
     # create the matrix of checkered squares off of the size 
     for x_count in range(0,x):
         for y_count in range(0,y):
-            currentRect = pygame.Rect(startPosition.x+ x_count*BG_SQUARE_LENGTH,
-            startPosition.y+y_count*BG_SQUARE_LENGTH,BG_SQUARE_LENGTH,BG_SQUARE_LENGTH)
-            if (x_count+ y_count) % 2 ==0:
+            currentRect = pygame.Rect(
+                startPosition.x + x_count * BG_SQUARE_LENGTH,
+                startPosition.y + y_count * BG_SQUARE_LENGTH,
+                BG_SQUARE_LENGTH,
+                BG_SQUARE_LENGTH
+            )
+            if (x_count + y_count) % 2 == 0:
                 charColor = BACKGROUND_CHECKER_BLUE
             else:
                 charColor = BACKGROUND_CHECKER_WHITE
--- main.py
+++ main.py
@@ -26,13 +26,17 @@ COIN_IMG = pygame.transform.scale(coin_img, (48, 48))
 #Define our player sprites
 
 
+# TEST CODE - REMOVE ALL REFERENCES
+font = pygame.font.SysFont("comicsans", 20)
+txt = pygame.font.Font.render(font, "Level 1", True, (0, 0, 0))
+# END TEST CODE
 
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) # center by default
-movement = pygame.Vector2(0,0)
+movement = pygame.Vector2(0, 0)
 
 #initial coin position (before movement function in the loop)
-coin_position = pygame.Vector2(screen.get_width()/2,screen.get_height()/2)
+coin_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 match LEVEL: # change player spawn position based on the level number
     case 1:
@@ -42,7 +46,7 @@ match LEVEL: # change player spawn position based on the level number
 #GAME LOOP
 time = 0
 while running:
-    time= pygame.time.get_ticks() / 1000 
+    time = pygame.time.get_ticks() / 1000 
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill(LevelFunctions.BG_COLOR)
@@ -53,18 +57,38 @@ while running:
                 lvl1Width = 17
                 lvl1Height = 6
                 lvl1StartX = 100
-                lvl1StartY = (screen.get_height() / 2) -(LevelFunctions.BG_SQUARE_LENGTH*lvl1Height/2)
-                safeAreas = [pygame.Rect(-20,lvl1StartY,LevelFunctions.BG_SQUARE_LENGTH*2,LevelFunctions.BG_SQUARE_LENGTH*lvl1Height)]
-                finishArea = pygame.Rect(LevelFunctions.BG_SQUARE_LENGTH*lvl1Width + 100,lvl1StartY,LevelFunctions.BG_SQUARE_LENGTH*2,LevelFunctions.BG_SQUARE_LENGTH*lvl1Height) 
+                # centers the map height wise?
+                lvl1StartY = screen.get_height() / 2 - LevelFunctions.BG_SQUARE_LENGTH * lvl1Height / 2
+                safeAreas = [pygame.Rect(
+                    -20,
+                    lvl1StartY,
+                    LevelFunctions.BG_SQUARE_LENGTH * 2,
+                    LevelFunctions.BG_SQUARE_LENGTH * lvl1Height
+                )]
+                finishArea = pygame.Rect(
+                    LevelFunctions.BG_SQUARE_LENGTH*lvl1Width + 100,
+                    lvl1StartY,
+                    LevelFunctions.BG_SQUARE_LENGTH * 2,
+                    LevelFunctions.BG_SQUARE_LENGTH * lvl1Height
+                )
+
+                # creates the checkered background tiles of white/blue tiles 
                 LevelFunctions.create_level(
                     screen,
                     safeAreas,
                     finishArea,
                     pygame.Vector2(lvl1StartX,lvl1StartY),
                     lvl1Width,
-                    lvl1Height) # creates the checkered background tiles of white/blue tiles 
+                    lvl1Height
+                )
+
+                # TODO: REMOVE To truly center the text, set to sw/2 - tw/2
+                screen.blit(txt, (screen.get_width() / 2 - txt.get_width() / 2, screen.get_height() / 10))
+
             except Exception as E:
                 print(E)
+        case _:
+            print(f"LEVEL NOT FOUND")
 
     '''
     ---- IF YOU WANT TO RENDER ANYTHING NEW FOR LEVEL 1 (ENEMY CIRCLE,WALLS,etc.) DO IT HERE ---- 
@@ -83,13 +107,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+    # draws player sprite (use blit for sprite)
+    screen.blit(PLAYER_IMG, player_pos)
 
-    screen.blit(PLAYER_IMG, (player_pos.x, player_pos.y)) # draws player sprite (use blit for sprite)
+    # function to move coin in a sine wave 
+    coin_position.y= screen.get_height() / 2 + math.sin(time * 2.5) * 15 
 
-    coin_position.y= (screen.get_height()/2) + math.sin(time * 2.5) *15 # function to move coin in a sine wave 
-    screen.blit(COIN_IMG, (coin_position.x, coin_position.y)) #draws coin sprite 
+    # draws coin sprite
+    screen.blit(COIN_IMG, coin_position)  
 
-    movement = pygame.Vector2(0,0) # set the player movement vector to zero before handling inputs
+    # set the player movement vector to zero before handling inputs
+    movement = pygame.Vector2(0,0)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         movement.y -= 1
@@ -99,16 +128,17 @@ while running:
         movement.x -= 1 
     if keys[pygame.K_d]:
         movement.x += 1
-    if keys[pygame.K_m]: # if the user hits m, it exits out of the game (FOR DEBUGGING ONLY)
+    # if the user hits m, it exits out of the game (FOR DEBUGGING ONLY)
+    if keys[pygame.K_m]:
         running = False
 
 
     #when movement vector greater than 1 (for diagonals)
-    if(movement.length() > 1):
-        movement = movement.normalize()
+    if (movement.length() > 1):
+        movement.normalize_ip()
     
-    player_pos += movement * PLAYER_SPEED *dt
-   
+    player_pos += movement * PLAYER_SPEED * dt
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 

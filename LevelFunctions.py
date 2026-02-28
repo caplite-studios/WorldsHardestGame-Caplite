180d39d74804681839d78f4de1e76c58543a8966
LucasCunnane1623 <lucascunnane16@gmail.com>
2026-02-28 16:11:57 -0500

    new changes

--- LevelFunctions.py
+++ LevelFunctions.py
@@ -1,16 +1,39 @@
-import pygame
+import pygame as pg
+import os
+from PIL import Image
 
-BACKGROUND_CHECKER_BLUE = pygame.Color(224, 218, 254)
-BACKGROUND_CHECKER_WHITE = pygame.Color(249, 248, 255)
-BG_COLOR = pygame.Color(177,172,255)
-SAFE_AREA_COLOR = pygame.Color(114,222,110)
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+assets_dir = os.path.join(main_dir, "assets")
+
+
+BACKGROUND_CHECKER_BLUE = pg.Color(224, 218, 254)
+BACKGROUND_CHECKER_WHITE = pg.Color(249, 248, 255)
+BACKGROUND_BLACK = pg.Color(0,0,0)
+BACKGROUND_GREEN = pg.Color(0,230,0)
+BACKGROUND_COLOR = pg.Color(177,172,255)
+SAFE_AREA_COLOR = pg.Color(114,222,110)
 BG_SQUARE_LENGTH = 60 
+ORIENTATION_OFFSET_X = -300
+ORIENTATION_OFFSET_Y = -200
+
+def load_image(name, scale=1, size=None):
+    fullname = os.path.join(assets_dir, name)
+    image = pg.image.load(fullname).convert_alpha()
+    if size:
+        image = pg.transform.scale(image, size)
+    elif scale != 1:
+        size = image.get_size()
+        image = pg.transform.scale(image, (int(size[0] * scale), int(size[1] * scale)))
+    return image, image.get_rect()
+
+#load source images 
+#BG_SQ_BLACK = load_image('blackBox.png',1,(48,48))
 
 def create_level(
     screen,
     safeAreas,
     finishArea,
-    startPosition = pygame.Vector2(0,0),
+    startPosition = pg.Vector2(0,0),
     x=0,
     y=0
 ):
@@ -22,21 +45,59 @@ def create_level(
     # create the matrix of checkered squares off of the size 
     for x_count in range(0,x):
         for y_count in range(0,y):
-            currentRect = pygame.Rect(
+            currentRect = pg.Rect(
                 startPosition.x + x_count * BG_SQUARE_LENGTH,
                 startPosition.y + y_count * BG_SQUARE_LENGTH,
                 BG_SQUARE_LENGTH,
                 BG_SQUARE_LENGTH
             )
             if (x_count + y_count) % 2 == 0:
+                
                 charColor = BACKGROUND_CHECKER_BLUE
             else:
                 charColor = BACKGROUND_CHECKER_WHITE
-            pygame.draw.rect(screen,charColor,currentRect)
+            pg.draw.rect(screen,charColor,currentRect)
 
     #create the level spawn (safe area)
     for i in range(len(safeAreas)):
-        pygame.draw.rect(screen,SAFE_AREA_COLOR,safeAreas[i])
+        pg.draw.rect(screen,SAFE_AREA_COLOR,safeAreas[i])
     
     #create the level finish box (safe area) --> takes the player to the next level once all coins are collected
-    pygame.draw.rect(screen,SAFE_AREA_COLOR,finishArea)
+    pg.draw.rect(screen,SAFE_AREA_COLOR,finishArea)
+
+
+
+
+"""
+Given an image (32x32)
+0. Open the image 
+1. iterate through each pixel of the image  
+2. draw rect (size 60) at coordinate scaled up 
+"""
+def convertImageToScreen(screen=None, src=''):
+    if(screen ==None):
+        raise ValueError("[convertImageToScreen]  not given!")
+    if(src ==''):
+        raise ValueError("[convertImageToScreen] src not given!")
+    img = Image.open(src).convert("RGBA")
+    width,height =  img.size
+    pixels = img.load()
+    returnList = []
+    for x in range(width):
+        for y in range(height):
+            pixel_value  = pixels[x,y]
+            r,g,b,a = pixel_value
+
+            if(a== 0): # pixel is casted to a transparent pixel
+                continue 
+                
+            #print(f'alpha of pixel ({x},{y}) = {a} ')
+            currentRect = pg.Rect(
+                ORIENTATION_OFFSET_X + x * BG_SQUARE_LENGTH,
+                ORIENTATION_OFFSET_Y + y * BG_SQUARE_LENGTH,
+                BG_SQUARE_LENGTH,
+                BG_SQUARE_LENGTH
+            )
+            currColor = pg.Color(r,g,b)
+            returnList.append((currColor,currentRect))
+    return returnList
--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import os
 import pygame as pg
 import math
-
-pg.init()
-screen = pg.display.set_mode((1280, 720))
-clock = pg.time.Clock()
-running = True
-dt = 0
+import LevelFunctions
 ########################################################
 # Define global variables 
 ########################################################
 LEVEL = 1
-
-import LevelFunctions
 PLAYER_COLOR = pg.Color(251,3,1)
 SPEED_INT = 4
 PLAYER_SPEED = SPEED_INT * 100
+SCREEN_WIDTH = 1440
+SCREEN_HEIGHT = SCREEN_WIDTH
+
 ########################################################
-# Define helper functions
+# Setting up screen display  
 ########################################################
+pg.init()
+screen = pg.display.set_mode((1440, 1440))
+clock = pg.time.Clock()
+running = True
+dt = 0
+
+# Define the background
+background = pg.Surface(screen.get_size()).convert()
+background.fill(LevelFunctions.BACKGROUND_COLOR)
+
+
+# Render blank background while loading happens
+screen.blit(background, (0, 0))
+pg.display.flip()
+
+
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 assets_dir = os.path.join(main_dir, "assets")
 
-def load_image(name, scale=1, size=None):
-    fullname = os.path.join(assets_dir, name)
-    image = pg.image.load(fullname).convert_alpha()
-    if size:
-        image = pg.transform.scale(image, size)
-    elif scale != 1:
-        size = image.get_size()
-        image = pg.transform.scale(image, (int(size[0] * scale), int(size[1] * scale)))
-    return image, image.get_rect()
+
 
 ########################################################
 # Define our player sprites
@@ -39,7 +43,7 @@ class Player(pg.sprite.Sprite):
 
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('Character.png', 1, (48, 48))
+        self.image, self.rect = LevelFunctions.load_image('Character.png', 1, (48, 48))
         self.rect.topleft = (x, y)
         self.pos = pg.math.Vector2(x, y)
         self.velocity = pg.math.Vector2(0, 0)
@@ -67,7 +71,7 @@ class Coin(pg.sprite.Sprite):
 
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('coin.png', 1, (48, 48))
+        self.image, self.rect = LevelFunctions.load_image('coin.png', 1, (48, 48))
         self.rect.topleft = (x, y)
         self.anchor_pos = pg.math.Vector2(x, y)
         self.pos = pg.math.Vector2(x, y)
@@ -77,6 +81,13 @@ class Coin(pg.sprite.Sprite):
         self.pos = pg.Vector2(self.anchor_pos.x, self.anchor_pos.y + math.sin(t * 2.5) * 15)
         self.rect.topleft = (int(self.pos.x), int(self.pos.y))
 
+class Wall(pg.sprite.Sprite):
+
+
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface(x, y)
+        self.rect = self.image.get_rect()
 
 ########################################################
 # Initialize Sprites
@@ -89,71 +100,22 @@ coin = Coin(screen.get_width()/2, screen.get_height()/2)
 
 allsprites = pg.sprite.RenderPlain((player, coin))
 
-match LEVEL: # change player spawn position based on the level number
-    case 1:
-        # spawn player in the middle of the screen, all the way to the left
-        player_pos = pg.Vector2(0, screen.get_height() / 2)
+# get all rectangles on screen from the pixel art image 
+rectsOnScreen = LevelFunctions.convertImageToScreen(screen,'./assets/level1map.png')
+newBg = pg.Surface(screen.get_size()).convert()
+newBg.fill(LevelFunctions.BACKGROUND_COLOR)
 
+for (color, rect) in rectsOnScreen:
+    pg.draw.rect(newBg, color, rect)
+
+    
 ########################################################
 #GAME LOOP
 ########################################################
-time = 0
 while running:
-    time = pg.time.get_ticks() / 1000 
-
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill(LevelFunctions.BG_COLOR)
+    screen.fill(LevelFunctions.BACKGROUND_COLOR)
     
-    match LEVEL: # define our level
-        case 1: # the user selects level 1 
-            try:
-                lvl1Width = 17
-                lvl1Height = 6
-                lvl1StartX = 100
-                # centers the map height wise?
-                lvl1StartY = screen.get_height() / 2 - LevelFunctions.BG_SQUARE_LENGTH * lvl1Height / 2
-                safeAreas = [pg.Rect(
-                    -20,
-                    lvl1StartY,
-                    LevelFunctions.BG_SQUARE_LENGTH * 2,
-                    LevelFunctions.BG_SQUARE_LENGTH * lvl1Height
-                )]
-                finishArea = pg.Rect(
-                    LevelFunctions.BG_SQUARE_LENGTH*lvl1Width + 100,
-                    lvl1StartY,
-                    LevelFunctions.BG_SQUARE_LENGTH * 2,
-                    LevelFunctions.BG_SQUARE_LENGTH * lvl1Height
-                )
-
-                # creates the checkered background tiles of white/blue tiles 
-                LevelFunctions.create_level(
-                    screen,
-                    safeAreas,
-                    finishArea,
-                    pg.Vector2(lvl1StartX,lvl1StartY),
-                    lvl1Width,
-                    lvl1Height
-                )
-
-                # TODO: REMOVE To truly center the text, set to sw/2 - tw/2
-                screen.blit(txt, (screen.get_width() / 2 - txt.get_width() / 2, screen.get_height() / 10))
-
-            except Exception as E:
-                print(E)
-        case _:
-            print(f"LEVEL NOT FOUND")
-
-    '''
-    ---- IF YOU WANT TO RENDER ANYTHING NEW FOR LEVEL 1 (ENEMY CIRCLE,WALLS,etc.) DO IT HERE ---- 
-    START TODO
-    '''
-
-
-
-    '''
-    END TODO 
-    '''
-
 
     # poll for events
     # pygame.QUIT event means the user clicked red X to close your window
@@ -165,9 +127,11 @@ while running:
     if keys[pg.K_m]:
         running = False
 
+    screen.blit(newBg,(0,0))
     allsprites.update()
 
     allsprites.draw(screen)
+
 
     # flip() the display to put your work on screen
     pg.display.flip()
